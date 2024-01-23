ace17623ee5fd373c7c4178b2f9cc59875fa9024
JZeedo <jameskstanton123@gmail.com>
2024-01-23 17:01:22 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 function getComputerChoice(){
     let choices = ["rock","paper","scissors"]
-    return choices[Math.floor(Math.random()*2)]
+    return choices[Math.floor(Math.random()*3)]
+}
+
+function playRound(playerSelection,computerSelection){
+    
+    if (playerSelection == "rock"){
+        switch(computerSelection){
+            case "paper":
+                return "You lose! Paper beats Rock";
+            case "scissors":
+                return "You win! Rock beats Scissors";
+            case "rock":
+                return "Tie!"
+        }
+    }
+    else if (playerSelection=="paper"){
+        switch(computerSelection){
+            case "scissors":
+                return "You lose! Scissors beats Paper";
+            case "rock":
+                return "You win! Paper beats Rock";
+            case "paper":
+                return "Tie!"
+        }
+    }
+    else if (playerSelection=="scissors"){
+        switch(computerSelection){
+            case "rock":
+                return "You lose! Rock beats Scissors";
+            case "paper":
+                return "You win! Scissors beats Paper";
+            case "rock":
+                return "Tie!"
+        }
+    }
+    else{
+        return "invalid input"
+    }
+
 }
